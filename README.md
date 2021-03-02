ea2f4eef5c16467cb8dd5a7481490eb187f054f0
caiolul <caiolucena.il@gmail.com>
2021-03-02 13:40:49 -0300

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# NutriJess blog
+
+Based on this [template](https://github.com/caiolul/nextjs-base).
+
+**Obs**
+Despite being created based on rocketseat changes can / will be made for better adaptation.
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+// Method for placing images
+// import Logo from '../dir/logo'
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>NutriJess</title>
+      </Head>
+      <main>
+        {/* <Logo/> */}
+        <h1>Nurtri jess</h1>
+      </main>
+    </div>
+  )
+}
+export default Home
