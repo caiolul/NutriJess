62143da4183d4ca920dffa0adbff8210321faed4
caiolul <caiolucena.il@gmail.com>
2021-03-08 16:08:03 -0300

    🛠fix: Change to react-bootstrap

--- src/components/NavContainer.tsx
+++ src/components/NavContainer.tsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
-import { Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
+import { Navbar } from 'react-bootstrap';
 
 const NavContainer: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <div>
-      <Navbar color="light" light fixed="top" shadow="true" expand="md">
-        <NavbarBrand href="/">NutriJess</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-      </Navbar>
-    </div>
+    <Navbar bg="light" expand="lg" fixed='top'>
+  <Navbar.Brand>NutriJess</Navbar.Brand>
+  {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+  {/* <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+    </Nav>
+  </Navbar.Collapse> */}
+</Navbar>
+    // <div>
+    //   <Navbar color="light" light fixed="top" shadow="true" expand="md">
+    //     <NavbarBrand href="/">NutriJess</NavbarBrand>
+    //     <NavbarToggler onClick={toggle} />
+    //   </Navbar>
+    // </div>
   );
 };
 export default NavContainer;
